785804cce8e2917ecee6ceeecb521b8a9ffecded
bmurdock <brian.j.murdock@gmail.com>
2021-03-05 19:43:39 -0700

    added marvel auth examples and whatnot

---
+++ apiAuth.js
@@ -0,0 +1,69 @@
+// This has nothing to do with Bitcoin
+const crypto = require('crypto');
+// Make fetch happen
+const fetch = require('node-fetch');
+
+const privKey = process.env.PRIVATE_KEY;
+const pubKey = process.env.PUBLIC_KEY;
+
+function auth()
+{
+    const ts = new Date().getTime();
+    const hash = crypto.createHash('md5').update(ts + privKey + pubKey).digest('hex');
+    console.log('hash: ', hash);
+
+    return {ts, hash};
+}
+
+function grab(resource, options)
+{
+    return new Promise((resolve, reject) =>
+    {
+        const apiKey = process.env.PUBLIC_KEY;
+        const base = 'https://gateway.marvel.com';
+        const {ts, hash} = auth();
+        let route = `${base}/v1/public/${resource}`;
+
+   
+        route += `?apikey=${apiKey}&ts=${ts}&hash=${hash}`;
+
+
+        // This is where things can get very complicated and where you can 
+        // do the most customization
+        if (options)
+        {
+            // we need to do something omg
+            if(options.name)
+            {
+                route += `&nameStartsWith=${options.name}`
+            }
+        }
+        console.log('route: ', route);
+        fetch(`${route}`)
+        .then((response) =>
+        {
+            return response.json();
+        }).
+        then((data) =>
+        {
+            if (data.code === 200)
+            {
+                resolve(data.data.results);
+            }
+            else
+            {
+                console.log('data: ', data);
+                // We probably should have a better explanation.
+                reject(new Error('For reasons..'));
+            }
+        })
+        .catch((err) =>
+        {
+            console.log('Error fetching: ', err);
+            reject(err);
+        });
+    })
+}
+
+exports.auth = auth;
+exports.grab = grab;
--- index.js
+++ index.js
@@ -2,12 +2,33 @@ const express = require('express');
 // import environment variables store in the .env file
 require('dotenv').config();
 
+const fetch = require('node-fetch');
+
+const {grab} = require('./apiAuth');
+
 const app = express();
 
 
 // handle the middleware stuffs
 app.use('/', express.static('public'));
 
+
+// go to http://localhost:portnumber/wolverine
+app.get('/wolverine', (req, res, next) =>
+{
+    grab('characters', {name: 'Wolverine'})
+    .then((data) =>
+    {
+        console.log('data: ', data);
+        res.json(data);
+    })
+    .catch((err) =>
+    {
+        console.log('Err: ', err);
+    })
+    
+
+});
 
 app.listen(process.env.PORT, () =>
 {
